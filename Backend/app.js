020124e0067dc9096911aff8a5ff29a2eb355d7b
Zach <sierrazja@gmail.com>
2024-12-04 13:58:50 -0600

    Added Backend code

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ const options = {
         },
         servers : [
             {
-                url: "https://ec2-18-227-140-139.us-east-2.compute.amazonaws.com:5000/"
+                url: "http://ec2-18-227-140-139.us-east-2.compute.amazonaws.com:5000/"
             }
         ]
     },
